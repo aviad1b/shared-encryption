ba17a61fa5570bf4c2f5c2fa6194e2faf901e22e
aviad1b <aviad.bloch10@gmail.com>
2025-11-04 04:42:04 +0200

    Feat: Added IP address classes implementations

--- senc/utils/Socket.cpp
+++ senc/utils/Socket.cpp
@@ -2,5 +2,21 @@
 
 namespace senc::utils
 {
+	IPv4::IPv4(const std::string& addr) : _addr(addr) { }
 
+	IPv4::IPv4(std::string&& addr) : _addr(std::move(addr)) { }
+
+	bool IPv4::operator==(const Self& other) const
+	{
+		return this->_addr == other._addr;
+	}
+
+	IPv6::IPv6(const std::string& addr) : _addr(addr) { }
+
+	IPv6::IPv6(std::string&& addr) : _addr(std::move(addr)) { }
+
+	bool IPv6::operator==(const Self& other) const
+	{
+		return this->_addr == other._addr;
+	}
 }
