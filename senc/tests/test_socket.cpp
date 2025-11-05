7ff467acf58b89619cf84a4b49c3ee2fb8519c6e
aviad1b <aviad.bloch10@gmail.com>
2025-11-05 07:08:35 +0200

    Test: Added TCP socket test

--- senc/tests/test_socket.cpp
+++ senc/tests/test_socket.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
 #include <thread>
+#include <future>
 #include "../utils/Socket.hpp"
 
 using senc::utils::UdpSocket;
+using senc::utils::TcpSocket;
 using senc::utils::IPv4;
 using senc::utils::IPv6;
 
@@ -28,5 +30,32 @@ TEST(SocketTests, TestUDP)
 	sock2.sendto(sendData, "127.0.0.1", 4350);
 
 	auto recvData = sock1.recvfrom(sendData.size());
+	EXPECT_EQ(sendData, recvData);
+}
+
+TEST(SocketTests, TestTCP)
+{
+	const std::vector<std::byte> sendData { std::byte(1), std::byte(2), std::byte(3) };
+	TcpSocket<IPv4> listen_sock, send_sock;
+	std::promise<TcpSocket<IPv4>> p;
+	std::future<TcpSocket<IPv4>> f = p.get_future();
+
+	listen_sock.bind(4350);
+	listen_sock.listen();
+
+	std::jthread t(
+		[&listen_sock, &p]()
+		{
+			try { p.set_value(listen_sock.accept()); }
+			catch (...) { p.set_exception(std::current_exception()); }
+		}
+	);
+
+	send_sock.connect("127.0.0.1", 4350);
+
+	TcpSocket<IPv4> recv_sock = f.get();
+
+	send_sock.send(sendData);
+	auto recvData = recv_sock.recv(sendData.size());
 	EXPECT_EQ(sendData, recvData);
 }
