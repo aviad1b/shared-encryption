92e9f9911243bc05c2bfdbdc4fcc977625400aa1
aviad1b <aviad.bloch10@gmail.com>
2025-11-05 11:54:29 +0200

    Chore: Added InputIterator concept

---
+++ senc/utils/concepts.hpp
@@ -0,0 +1,25 @@
+/*********************************************************************
+ * \file   concepts.hpp
+ * \brief  Contains general concept declarations.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Heshvan 5786
+ *********************************************************************/
+
+#pragma once
+
+#include <concepts>
+#include <iterator>
+
+namespace senc::utils
+{
+	/**
+	 * @concept senc::utils::InputIterator
+	 * @brief Looks for a typename that is an input iterator for a given element type.
+	 * @tparam Self Examined typename.
+	 * @tparam T Element type.
+	 */
+	template <typename Self, typename T>
+	concept InputIterator = std::input_iterator<Self> &&
+		std::same_as<typename Self::value_type, T>;
+}
