370a520c85b543fc2af8861ad3a9f10db4389d6e
aviad1b <aviad.bloch10@gmail.com>
2025-11-05 11:35:23 +0200

    Chore: Added PolyCoeff concept

--- senc/utils/poly.hpp
+++ senc/utils/poly.hpp
@@ -8,9 +8,21 @@
 
 #pragma once
 
+#include <concepts>
+
 namespace senc::utils
 {
-
+	/**
+	 * @concept senc::utils::PolyCoeff
+	 * @brief Looks for a typename that can be used as a polynom coefficient.
+	 * @tparam Self Examined typename.
+	 */
+	template <typename Self>
+	concept PolyCoeff = requires
+	{
+		{ std::declval<Self>() + std::declval<Self>() } -> std::same_as<Self>;
+		{ std::declval<Self>() * std::declval<int>() } -> std::same_as<Self>;
+	};
 }
 
 #include "poly_impl.hpp"
