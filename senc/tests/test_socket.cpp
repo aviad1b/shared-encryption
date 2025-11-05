62118862db50c8307ea4fd2f98459e9a32889234
aviad1b <aviad.bloch10@gmail.com>
2025-11-05 02:49:29 +0200

    Test: Added UDP test

--- senc/tests/test_socket.cpp
+++ senc/tests/test_socket.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
+#include <thread>
 #include "../utils/Socket.hpp"
 
+using senc::utils::UdpSocket;
 using senc::utils::IPv4;
 using senc::utils::IPv6;
 
@@ -14,4 +16,18 @@ TEST(SocketTests, TestIPv6)
 {
 	IPv6 ip("fd30:cb0a:c87a:0157:a1b2:c3d4:e5f6:7890");
 	EXPECT_EQ(ip.as_str(), "fd30:cb0a:c87a:0157:a1b2:c3d4:e5f6:7890");
+}
+
+TEST(SocketTests, TestUDP)
+{
+	const std::vector<std::byte> sendData { std::byte(1), std::byte(2), std::byte(3) };
+	UdpSocket<IPv4> sock1, sock2;
+
+	sock1.bind(435);
+	sock2.bind(436);
+
+	sock2.sendto(sendData, "127.0.0.1", 435);
+
+	auto recvData = sock1.recvfrom(sendData.size(), "127.0.0.1", 436);
+	EXPECT_EQ(sendData, recvData);
 }
