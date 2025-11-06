e2b7f279b712bd05f7ac87a74571b07fa679f802
aviad1b <aviad.bloch10@gmail.com>
2025-11-06 05:43:28 +0200

    Chore: Added SelfMultiplicable concept

--- senc/utils/math.hpp
+++ senc/utils/math.hpp
@@ -11,6 +11,17 @@
 
 namespace senc::utils
 {
+	/**
+	 * @concept senc::utils::SelfMultiplicable
+	 * @brief Looks for a typename that can be multiplied by itself.
+	 * @tparam Self Examined typename.
+	 */
+	template <typename Self>
+	concept SelfMultiplicable = requires(Self self)
+	{
+		{ self *= std::declval<Self>() } -> std::same_as<Self&>;
+	};
+
 	template <typename T>
 	requires std::is_fundamental_v<T>
 	inline T pow(T val, int exp)
