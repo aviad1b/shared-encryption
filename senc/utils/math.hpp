5f5ad654177fec94444905aa52ed5aa160acada5
aviad1b <aviad.bloch10@gmail.com>
2025-11-06 05:50:28 +0200

    Chore: Added HasPowMethod concept

--- senc/utils/math.hpp
+++ senc/utils/math.hpp
@@ -28,6 +28,17 @@ namespace senc::utils
 		{ self *= std::declval<Self>() } -> std::same_as<Self&>;
 	};
 
+	/**
+	 * @concept senc::utils::HasPowMethod
+	 * @brief Looks for a typename with a power-computing method.
+	 * @tparam Self Examined typename.
+	 */
+	template <typename Self>
+	concept HasPowMethod = requires(const Self self)
+	{
+		{ self.pow(std::declval<Exponent>()) } -> std::same_as<Self>;
+	};
+
 	template <typename T>
 	requires std::is_fundamental_v<T>
 	inline T pow(T val, Exponent exp)
