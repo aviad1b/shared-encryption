5dd87a2f2ff235e6237fe6ab895c6cdca6cb2f5a
aviad1b <aviad.bloch10@gmail.com>
2025-11-08 00:02:24 +0200

    Feat: Added random distribution functions

--- senc/utils/Random.hpp
+++ senc/utils/Random.hpp
@@ -8,11 +8,31 @@
 
 #pragma once
 
+#include <functional>
 #include <concepts>
 #include <random>
 #include <chrono>
 
 namespace senc::utils
 {
-	
+	/**
+	 * @class senc::utils::Random
+	 * @tparam T Integer type to sample.
+	 */
+	template <std::integral T>
+	class Random
+	{
+	public:
+		Random() = delete; // prevent instancing
+
+		/**
+		 * @brief Gets a sample distribution within a given range [min, max].
+		 */
+		static std::function<T()> get_range_dist(T min, T max);
+
+		/**
+		 * @brief Gets a non-negative sample distribution below a given upper bound.
+		 */
+		static std::function<T()> get_dist_below(T upperBound);
+	};
 }
--- senc/utils/Random_impl.hpp
+++ senc/utils/Random_impl.hpp
@@ -5,3 +5,26 @@
  * \author test
  * \date   November 2025
  *********************************************************************/
+
+#include "Random.hpp"
+
+namespace senc::utils
+{
+	template <std::integral T>
+	inline std::function<T()> Random<T>::get_range_dist(T min, T max)
+	{
+		return [dist = std::uniform_int_distribution<T>(min, max)]() mutable
+		{
+            static thread_local std::mt19937 engine(
+                std::chrono::high_resolution_clock::now().time_since_epoch().count()
+            );
+            return dist(engine);
+        };
+	}
+	
+	template <std::integral T>
+	inline std::function<T()> Random<T>::get_dist_below(T upperBound)
+	{
+		return get_range_dist(0, upperBound - 1);
+	}
+}
