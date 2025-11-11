2baf547e8bf359b2c9063b2c2e10e189698097ec
aviad1b <aviad.bloch10@gmail.com>
2025-11-11 13:41:15 +0200

    Chore: Added ECGroup files to project

---
+++ senc/utils/ECGroup.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * \file   ECGroup.cpp
+ * \brief  Implementation of ECGroup class.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Heshvan 5786
+ *********************************************************************/
+
+#include "ECGroup.hpp"
+
+namespace senc::utils
+{
+
+}
---
+++ senc/utils/ECGroup.hpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * \file   ECGroup.hpp
+ * \brief  Header of ECGroup class.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Heshvan 5786
+ *********************************************************************/
+
+#pragma once
+
+namespace senc::utils
+{
+
+}
