97e23340ad6756b9f19258e7b380999d96913dda
aviad1b <aviad.bloch10@gmail.com>
2025-11-14 07:06:16 +0200

    Feat: Implemented HybridElGamal2L encryption

--- senc/utils/enc/HybridElGamal2L_impl.hpp
+++ senc/utils/enc/HybridElGamal2L_impl.hpp
@@ -29,4 +29,23 @@ namespace senc::utils::enc
 		PubKey pubKey = senc::utils::pow(G::GENERATOR, privKey);
 		return { pubKey, privKey };
 	}
+
+	template <Group G, Symmetric1L S, ConstCallable<Key<S>, G, G> KDF>
+	inline HybridElGamal2L<G, S, KDF>::Ciphertext HybridElGamal2L<G, S, KDF>::encrypt(const Plaintext& plaintext, const PubKey& pubKey1, const PubKey& pubKey2) const
+	{
+		auto r1 = UNDER_ORDER_DIST();
+		auto r2 = UNDER_ORDER_DIST();
+
+		auto c1 = senc::utils::pow(G::GENERATOR, r1);
+		auto c2 = senc::utils::pow(G::GENERATOR, r2);
+
+		auto z1 = senc::utils::pow(pubKey1, r1);
+		auto z2 = senc::utils::pow(pubKey2, r2);
+
+		auto k = this->_kdf(z1, z2);
+
+		auto c3 = this->_symmetricSchema.encrypt(plaintext, k);
+
+		return { c1, c2, c3 };
+	}
 }
