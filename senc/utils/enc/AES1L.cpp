06cadbd9e846b238ee8b80ad09a92d778a3102a3
aviad1b <aviad.bloch10@gmail.com>
2025-11-15 21:53:50 +0200

    Feat: Implemented AES1L encryption

--- senc/utils/enc/AES1L.cpp
+++ senc/utils/enc/AES1L.cpp
@@ -1,5 +1,9 @@
 #include "AES1L.hpp"
 
+#include <cryptopp/filters.h>
+#include <cryptopp/modes.h>
+#include <cryptopp/aes.h>
+
 namespace senc::utils::enc
 {
 	AES1L::Key AES1L::keygen() const
@@ -7,5 +11,25 @@ namespace senc::utils::enc
 		CryptoPP::SecByteBlock res(CryptoPP::AES::DEFAULT_KEYLENGTH);
 		_prng.GenerateBlock(res, res.size());
 		return res;
+	}
+
+	AES1L::Ciphertext AES1L::encrypt(const Plaintext& plaintext, const Key& key)
+	{
+		CryptoPP::SecByteBlock cipherIV(CryptoPP::AES::BLOCKSIZE);
+		_prng.GenerateBlock(cipherIV, cipherIV.size());		
+
+		CryptoPP::CBC_Mode<CryptoPP::AES>::Encryption encryptor;
+		encryptor.SetKeyWithIV(key, key.size(), cipherIV);
+
+		// write binary output into cipherData:
+		std::vector<CryptoPP::byte> cipherData;
+		CryptoPP::ArraySource(
+			plaintext.data(), plaintext.size(),
+			true,
+			new CryptoPP::StreamTransformationFilter(
+				encryptor,
+				new CryptoPP::VectorSink(cipherData)
+			)
+		);
 	}
 }
