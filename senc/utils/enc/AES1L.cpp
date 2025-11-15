b6daedcd84df90d6c49d272ed0870c4ce4f35d83
aviad1b <aviad.bloch10@gmail.com>
2025-11-15 21:42:20 +0200

    Feat: Implemented AES1L key generation

--- senc/utils/enc/AES1L.cpp
+++ senc/utils/enc/AES1L.cpp
@@ -2,5 +2,10 @@
 
 namespace senc::utils::enc
 {
-
+	AES1L::Key AES1L::keygen() const
+	{
+		CryptoPP::SecByteBlock res(CryptoPP::AES::DEFAULT_KEYLENGTH);
+		_prng.GenerateBlock(res, res.size());
+		return res;
+	}
 }
