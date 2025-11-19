7912a2f486a0462db24b4d6dbe1b0d60a34adcf7
aviad1b <aviad.bloch10@gmail.com>
2025-11-19 06:45:21 +0200

    Chore: Added shamir.hpp and shamir_impl.hpp

---
+++ senc/utils/shamir.hpp
@@ -0,0 +1,16 @@
+/*********************************************************************
+ * \file   shamir.hpp
+ * \brief  Header of Shamir secret sharing utilities.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Heshvan 5786
+ *********************************************************************/
+
+#pragma once
+
+namespace senc::utils::shamir
+{
+
+}
+
+#include "shamir_impl.hpp"
---
+++ senc/utils/shamir_impl.hpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * \file   shamir_impl.hpp
+ * \brief  Template implementation of Shamir secret sharing utilities.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Heshvan 5786
+ *********************************************************************/
+
+#include "shamir.hpp"
+
+namespace senc::utils::shamir
+{
+
+}
