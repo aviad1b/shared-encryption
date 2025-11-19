1a00fb066cc99c84bcc62f1c04e036de4a778022
aviad1b <aviad.bloch10@gmail.com>
2025-11-19 04:44:38 +0200

    Feat: Implemented ECHKDF2L

--- senc/utils/enc/ECHKDF2L.cpp
+++ senc/utils/enc/ECHKDF2L.cpp
@@ -6,7 +6,43 @@
  * \date   November 2025, Heshvan 5786
  *********************************************************************/
 
+#include "ECHKDF2L.hpp"
+
 namespace senc::utils::enc
 {
+	ECHKDF2L::ECHKDF2L() : Self(DEFAULT_IKM_EACH_SIZE) { }
 
+	ECHKDF2L::ECHKDF2L(std::size_t ikmEachSize)
+		: Self(ikmEachSize, DEFAULT_SALT) { }
+
+	ECHKDF2L::ECHKDF2L(const Buffer& salt)
+		: Self(DEFAULT_IKM_EACH_SIZE, salt) { }
+
+	ECHKDF2L::ECHKDF2L(std::size_t ikmEachSize, const Buffer& salt)
+		: _ikmEachSize(ikmEachSize), _salt(salt) { }
+
+	AES1L::Key senc::utils::enc::ECHKDF2L::operator()(const ECGroup& a, const ECGroup& b) const
+	{
+		const auto& aNum = a.x();
+		const auto& bNum = b.x();
+
+		const std::size_t aSize = aNum.MinEncodedSize();
+		const std::size_t bSize = bNum.MinEncodedSize();
+
+		// make (EACH_SIZE * 2)-byte input keying material for HKDF out of aNum and bNum:
+		CryptoPP::SecByteBlock ikm(_ikmEachSize * 2);
+		aNum.Encode(ikm + (_ikmEachSize - aSize), aSize);
+		bNum.Encode(ikm + _ikmEachSize + (_ikmEachSize - bSize), bSize);
+
+		AES1L::Key key(AES1L::KEY_SIZE);
+		CryptoPP::HKDF<CryptoPP::SHA256> hkdf;
+		hkdf.DeriveKey(
+			key.data(), key.size(),
+			ikm.data(), ikm.size(),
+			_salt.data(), _salt.size(),
+			nullptr, 0
+		);
+
+		return key;
+	}
 }
