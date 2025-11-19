5a465202627206654fa26e10b01127dbc2f4bbff
aviad1b <aviad.bloch10@gmail.com>
2025-11-19 02:00:06 +0200

    Test: Added AES tests

---
+++ senc/tests/test_enc.cpp
@@ -0,0 +1,33 @@
+/*********************************************************************
+ * \file   test_enc.cpp
+ * \brief  Contains tests for encryption utilities.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Heshvan 5786
+ *********************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "../utils/enc/AES1L.hpp"
+#include "../utils/bytes.hpp"
+
+using senc::utils::enc::AES1L;
+using senc::utils::Buffer;
+
+struct EncDecTest : testing::Test, testing::WithParamInterface<Buffer> { };
+
+TEST_P(EncDecTest, AES)
+{
+	AES1L schema;
+	const auto key = schema.keygen();
+	const Buffer& data = GetParam();
+	const auto encrypted = schema.encrypt(data, key);
+	const auto decrypted = schema.decrypt(encrypted, key);
+	EXPECT_EQ(data, decrypted);
+}
+
+INSTANTIATE_TEST_CASE_P(AES, EncDecTest, testing::Values(
+	Buffer({ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }),
+	Buffer({ 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 }),
+	Buffer({ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF })
+));
