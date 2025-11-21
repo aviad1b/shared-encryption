ff60bb9258ba59ae9c5666b91f8988ad92e8f957
aviad1b <aviad.bloch10@gmail.com>
2025-11-21 07:30:34 +0200

    Fix: Added missing capture

--- senc/utils/Shamir_impl.hpp
+++ senc/utils/Shamir_impl.hpp
@@ -104,7 +104,7 @@ namespace senc::utils
 			shardsIDs |
 			views::enumerate | // p = pair{j, xj}
 			std::views::filter([i](auto p) { return p.first != i; }) | // filter where index isn't i
-			std::views::transform([](auto p) { return -p.second / xi - p.second; })
+			std::views::transform([xi](auto p) { return -p.second / xi - p.second; })
 		).value();
 	}
 }
