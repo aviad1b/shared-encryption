9ffdcb199176bb9ac64a6f7a728e786630df769a
aviad1b <aviad.bloch10@gmail.com>
2025-11-22 07:27:19 +0200

    Doc: Added TODO comment

--- senc/utils/Shamir_impl.hpp
+++ senc/utils/Shamir_impl.hpp
@@ -91,6 +91,7 @@ namespace senc::utils
 	inline typename Shamir<S, SID>::PackedSecret Shamir<S, SID>::get_lagrange_coeff(
 		std::size_t i, const std::vector<SID>& shardsIDs)
 	{
+		// TODO: This check may lower performance, is it REALLY necessary?
 		std::unordered_set<SID> shardsIDsSet;
 		for (const SID& shardID : shardsIDs)
 		{
