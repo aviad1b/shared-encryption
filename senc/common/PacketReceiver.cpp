e91fed9ebe3f46b1d9adf60acf01baff2338ac58
aviad1b <aviad.bloch10@gmail.com>
2025-11-26 22:32:30 +0200

    Chore: Added cpp file for PacketReceiver

---
+++ senc/common/PacketReceiver.cpp
@@ -0,0 +1,12 @@
+/*********************************************************************
+ * \file   PacketReceiver.cpp
+ * \brief  Non-template implementation of PacketReceiver class.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Kislev 5786
+ *********************************************************************/
+
+namespace senc
+{
+
+}
