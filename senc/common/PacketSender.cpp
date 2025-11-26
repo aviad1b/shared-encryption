ad2d1688159ca549518f44bd0f5ada3bee670106
aviad1b <aviad.bloch10@gmail.com>
2025-11-26 04:48:05 +0200

    Chore: Reorganized files for adding PackedSender

---
+++ senc/common/PacketSender.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * \file   PacketSender.cpp
+ * \brief  Implementation of PacketSender class.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Kislev 5786
+ *********************************************************************/
+
+#include "PacketSender.hpp"
+
+namespace senc
+{
+
+}
---
+++ senc/common/PacketSender.hpp
@@ -0,0 +1,16 @@
+/*********************************************************************
+ * \file   PacketSender.hpp
+ * \brief  Header of PacketSender class.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Kislev 5786
+ *********************************************************************/
+
+#pragma once
+
+#include "packets.hpp"
+
+namespace senc
+{
+
+}
