d813b4f66ec1d955b80920a734035cfd578e0782
aviad1b <aviad.bloch10@gmail.com>
2025-11-26 22:16:32 +0200

    Chore: Added declaration of PacketReceiver class

---
+++ senc/common/PacketReceiver.hpp
@@ -0,0 +1,28 @@
+/*********************************************************************
+ * \file   PacketReceiver.hpp
+ * \brief  Header of PacketSender class.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Kislev 5786
+ *********************************************************************/
+
+#pragma once
+
+#include "packets.hpp"
+
+namespace senc
+{
+	class PacketReceiver
+	{
+	public:
+		PacketReceiver() = delete;
+
+		template <pkt::Request T>
+		static inline T recv_request() { static_assert(false, "Unhandled request detected"); }
+
+		template <pkt::Response T>
+		static inline T recv_response() { static_assert(false, "Unhandled response detected"); }
+	};
+}
+
+#include "PacketReceiver_impl.hpp"
---
+++ senc/common/PacketReceiver_impl.hpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * \file   PacketReceiver_impl.hpp
+ * \brief  Implementation of PacketReceiver class.
+ *
+ * \author aviad1b
+ * \date   November 2025, Kislev 5786
+ *********************************************************************/
+
+#include "PacketReceiver.hpp"
+
+namespace senc
+{
+
+}
