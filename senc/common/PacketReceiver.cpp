76f8ce3cfe9219404a97a80260700e88a8e216d6
aviad1b <aviad.bloch10@gmail.com>
2025-11-26 22:44:45 +0200

    Feat: Implemented recv_big_int

--- senc/common/PacketReceiver.cpp
+++ senc/common/PacketReceiver.cpp
@@ -1,3 +1,4 @@
+#include "PacketReceiver.hpp"
 /*********************************************************************
  * \file   PacketReceiver.cpp
  * \brief  Non-template implementation of PacketReceiver class.
@@ -8,5 +9,10 @@
 
 namespace senc
 {
-
+	void PacketReceiver::recv_big_int(utils::Socket& sock, utils::BigInt& out)
+	{
+		const bigint_size_t size = sock.recv_connected_primitive<bigint_size_t>();
+		utils::Buffer buff = sock.recv_connected_exact(size);
+		out.Decode(buff.data(), buff.size());
+	}
 }
