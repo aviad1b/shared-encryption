b45bc6e9f56ae514a02cb9158153f33cd8a5dd52
aviad1b <aviad.bloch10@gmail.com>
2025-11-27 06:12:24 +0200

    Feat: Implemented recv_ciphertext

--- senc/common/PacketReceiver.cpp
+++ senc/common/PacketReceiver.cpp
@@ -8,6 +8,8 @@
 
 #include "PacketReceiver.hpp"
 
+#include "../utils/ranges.hpp"
+
 namespace senc
 {
 	void PacketReceiver::recv_big_int(utils::Socket& sock, utils::BigInt& out)
@@ -34,5 +36,36 @@ namespace senc
 		utils::BigInt second;
 		recv_big_int(sock, second);
 		out.second = second;
+	}
+
+	void PacketReceiver::recv_ciphertext(utils::Socket& sock, Ciphertext& out)
+	{
+		auto& [c1, c2, c3] = out;
+		auto& [c3a, c3b] = c3;
+
+		// recv size dividers
+		std::array<utils::BigInt, 4> bigintValues{};
+		std::array<bigint_size_t, 4> bigintSizes{};
+		buffer_size_t c3aSize = 0, c3bSize = 0;
+		for (auto& size : bigintSizes)
+			sock.recv_connected_value(size);
+		sock.recv_connected_value(c3aSize);
+		sock.recv_connected_value(c3bSize);
+
+		// recv actual data:
+
+		// bigints: use a one-size-fits-each buffer
+		utils::Buffer buff(*std::max_element(bigintSizes.begin(), bigintSizes.end()));
+		for (auto& [bigintValue, bigintSize] : utils::views::zip(bigintValues, bigintSizes))
+		{
+			sock.recv_connected_exact_into(buff.data(), bigintSize);
+			bigintValue.Encode(buff.data(), bigintSize);
+		}
+
+		// c3: reserve space then read directly from socket
+		c3a.resize(c3aSize);
+		sock.recv_connected_exact_into(c3a);
+		c3b.resize(c3bSize);
+		sock.recv_connected_exact_into(c3b);
 	}
 }
