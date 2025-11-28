4d614baf177164c2977549578dc226423359490a
aviad1b <aviad.bloch10@gmail.com>
2025-11-28 05:17:09 +0200

    Refactor: Modified PacketReceiver to take packet code into account, and accept multiple (potential) packet types

--- senc/common/PacketReceiver.hpp
+++ senc/common/PacketReceiver.hpp
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <optional>
+#include "../utils/variants.hpp"
 #include "../utils/Socket.hpp"
 #include "packets.hpp"
 
@@ -19,6 +21,30 @@ namespace senc
 		using Self = PacketReceiver;
 
 		virtual ~PacketReceiver() { }
+
+		/**
+		 * @brief Receives a request of one of the given types.
+		 * @tparam Ts Potential packet types (structs).
+		 * @return Received packet, or `std::nullopt` if was of wrong type.
+		 * @note Assuming any valid packet was sent, `sock` is guarenteed to be clear after call.
+		 */
+		template <typename... Ts>
+		inline std::optional<utils::VariantOrSingular<Ts...>> recv_request(utils::Socket& sock)
+		{
+			return recv_packet<PacketKind::Request, Ts...>(sock);
+		}
+
+		/**
+		 * @brief Receives a response of one of the given types.
+		 * @tparam Ts Potential packet types (structs).
+		 * @return Received packet, or `std::nullopt` if was of wrong type.
+		 * @note Assuming any valid packet was sent, `sock` is guarenteed to be clear after call.
+		 */
+		template <typename... Ts>
+		inline std::optional<utils::VariantOrSingular<Ts...>> recv_response(utils::Socket& sock)
+		{
+			return recv_packet<PacketKind::Response, Ts...>(sock);
+		}
 
 		virtual void recv_response_data(utils::Socket& sock, pkt::ErrorResponse& out) = 0;
 
@@ -52,20 +78,50 @@ namespace senc
 		virtual void recv_request_data(utils::Socket& sock, pkt::SendDecryptionPartRequest& out) = 0;
 		virtual void recv_response_data(utils::Socket& sock, pkt::SendDecryptionPartResponse& out) = 0;
 
-		template <typename T>
-		inline T recv_request(utils::Socket& sock)
+	private:
+		/**
+		 * @enum senc::PacketReceiver::PacketKind
+		 * @brief For internal use; Signified request or response.
+		 */
+		enum class PacketKind : std::uint8_t { Request, Response };
+
+		/**
+		 * @brief Receives data for specific packet.
+		 * @tparam kind Whether should receive request or response.
+		 * @tparam T Type of packet being received (packet struct).
+		 * @return Received packet.
+		 * @note Assumes valid input.
+		 */
+		template <PacketKind kind, typename T>
+		inline T recv_packet_data(utils::Socket& sock)
 		{
-			T req{};
-			this->recv_request_data(sock, req);
-			return req;
+			T ret{};
+			if constexpr (PacketKind::Request == kind)
+				recv_request_data(sock, ret);
+			else
+				recv_response_data(sock, ret);
+			return ret;
 		}
 
-		template <typename T>
-		inline T recv_response(utils::Socket& sock)
+		/**
+		 * @brief Receives a packet of one of the given types.
+		 * @tparam kind Whether should receive request or response.
+		 * @tparam Ts Potential packet types (structs).
+		 * @return Received packet, or `std::nullopt` if was of wrong type.
+		 * @note Assuming any valid packet was sent, `sock` is guarenteed to be clear after call.
+		 */
+		template <PacketKind kind, typename... Ts>
+		inline std::optional<utils::VariantOrSingular<Ts...>> recv_packet(utils::Socket& sock)
 		{
-			T resp{};
-			this->recv_response_data(sock, resp);
-			return resp;
+			std::optional<utils::VariantOrSingular<Ts...>> ret;
+			const auto code = sock.recv_connected_primitive<pkt::Code>();
+			(( // repeat for each type in Ts
+				Ts::CODE == code ? ( // if type's CODE is same as code
+					ret.emplace(recv_packet_data<kind, Ts>(sock)),
+					true
+				) : false // end if type's CODE is same as code
+			)...); // end repeat for each type in Ts
+			return ret;
 		}
 	};
 }
