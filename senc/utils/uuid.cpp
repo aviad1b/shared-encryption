7290dfc99cdfddb78358fabb32428363ad54c682
aviad1b <aviad.bloch10@gmail.com>
2025-11-29 21:43:48 +0200

    Chore: Added declaration of UUID class

---
+++ senc/utils/uuid.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * \file   uuid.cpp
+ * \brief  Implementation of UUID utilities.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Kislev 5786
+ *********************************************************************/
+
+#include "uuid.hpp"
+
+namespace senc::utils
+{
+
+}
---
+++ senc/utils/uuid.hpp
@@ -0,0 +1,61 @@
+/*********************************************************************
+ * \file   uuid.hpp
+ * \brief  Header of UUID utilities.
+ * 
+ * \author aviad1b
+ * \date   November 2025, Kislev 5786
+ *********************************************************************/
+
+#pragma once
+
+#include <ostream>
+#include <string>
+#include <rpc.h>
+
+namespace senc::utils
+{
+	class UUID
+	{
+	public:
+		using Self = UUID;
+
+		/**
+		 * @brief Constructs a zero-value UUID.
+		 */
+		UUID();
+
+		/**
+		 * @brief Constructs a UUID from a given string representation.
+		 * @param value String representation of UUID value.
+		 */
+		UUID(const std::string& value);
+		
+		/**
+		 * @brief Generates a (random) UUID.
+		 * @return Generated UUID.
+		 */
+		static Self generate();
+
+		/**
+		 * @brief Compares this UUID to another.
+		 * @param other Other UUID to compare to.
+		 * @return `true` if `*this` is the same UUID as `other`, otherwise `false`.
+		 */
+		bool operator==(const Self& other) const noexcept;
+
+		/**
+		 * @brief Gets string representation of UUID.
+		 * @return String representation of UUID.
+		 */
+		const std::string& to_string() const noexcept;
+
+
+		friend std::ostream& operator<<(std::ostream& os, const UUID& uuid);
+
+	private:
+		::UUID _value{};
+		std::string _str;
+
+		UUID(const ::UUID& value);
+	};
+}
