7a184aa5a4947087c0aaaf03d566ef42c73d2eb5
aviad1b <aviad.bloch10@gmail.com>
2025-12-02 02:09:48 +0200

    Chore: Added declaration of ShortTermServerStorage

---
+++ senc/server/ShortTermServerStorage.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * \file   ShortTermServerStorage.cpp
+ * \brief  Implementation of ShortTermServerStorage class.
+ * 
+ * \author aviad1b
+ * \date   December 2025, Kislev 5786
+ *********************************************************************/
+
+#include "ShortTermServerStorage.hpp"
+
+namespace senc::server
+{
+
+}
