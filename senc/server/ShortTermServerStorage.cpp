e85c181e9b4dad2e0e04b45385f8a982e7c90ce3
aviad1b <aviad.bloch10@gmail.com>
2025-12-02 02:39:15 +0200

    Feat: Refactored new_operation to use UUID generate unique static method

--- senc/server/ShortTermServerStorage.cpp
+++ senc/server/ShortTermServerStorage.cpp
@@ -12,17 +12,9 @@ namespace senc::server
 {
 	OperationID ShortTermServerStorage::new_operation(const std::string& requester)
 	{
-		OperationID opid = OperationID::generate();
-		{
-			const std::lock_guard<std::mutex> lock(_mtxOperations);
-
-			// make sure operation ID is unique
-			while (_operations.contains(opid))
-				opid = OperationID::generate();
-
-			// register operation
-			_operations.insert(std::make_pair(opid, requester));
-		}
+		const std::lock_guard<std::mutex> lock(_mtxOperations);
+		auto opid = OperationID::generate([this](auto x) { return _operations.contains(x); });
+		_operations.insert(std::make_pair(opid, requester));
 		return opid;
 	}
 
