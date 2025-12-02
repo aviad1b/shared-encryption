cc174e106980424f660291a6683f4b30705e034d
aviad1b <aviad.bloch10@gmail.com>
2025-12-03 01:54:53 +0200

    Chore: Added basic Server declarations

---
+++ senc/server/Server.cpp
@@ -0,0 +1,14 @@
+/*********************************************************************
+ * \file   Server.cpp
+ * \brief  Implementation of Server class.
+ * 
+ * \author aviad1b
+ * \date   December 2025, Kislev 5786
+ *********************************************************************/
+
+#include "Server.hpp"
+
+namespace senc::server
+{
+
+}
---
+++ senc/server/Server.hpp
@@ -0,0 +1,67 @@
+/*********************************************************************
+ * \file   Server.hpp
+ * \brief  Header of Server class.
+ * 
+ * \author aviad1b
+ * \date   December 2025, Kislev 5786
+ *********************************************************************/
+
+#pragma once
+
+#include "../common/PacketReceiver.hpp"
+#include "../common/PacketSender.hpp"
+#include "../utils/Socket.hpp"
+#include "IServerStorage.hpp"
+
+namespace senc::server
+{
+	/**
+	 * @brief Server class, manages server logic.
+	 */
+	class Server
+	{
+	public:
+		using Self = Server;
+		using Socket = utils::TcpSocket<utils::IPv4>;
+
+		/**
+		 * @brief Constructs a new server instance.
+		 * @param storage Implementation of `IServerStorage`.
+		 * @param receiver Implementation of `PacketReceiver`.
+		 * @param sender Implementation of `PacketSender`.
+		 * @note `storage`, `receiver` and `sender` are all assumed to be thread-safe.
+		 */
+		explicit Server(utils::Port listenPort,
+						IServerStorage& storage,
+						PacketReceiver& receiver,
+						PacketSender& sender);
+
+		/**
+		 * @brief Starts the server (without waiting).
+		 */
+		void start();
+
+		/**
+		 * @brief Stops server sun.
+		 */
+		void stop();
+
+		/**
+		 * @brief Waits for server to stop running.
+		 */
+		void wait();
+
+	private:
+		utils::Port _listenPort;
+		IServerStorage& storage;
+		PacketReceiver& receiver;
+		PacketSender& sender;
+		bool _isRunning;
+
+		/**
+		 * @brief Handles a newly connected client, until it disconnects.
+		 * @param sock Socket connected to client (moved).
+		 */
+		void handle_new_client(Socket sock);
+	};
+}
