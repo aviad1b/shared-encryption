7325518fa372ad5f2ce76aa6db010795175f6420
aviad1b <aviad.bloch10@gmail.com>
2025-12-02 02:22:20 +0200

    Feat: Implemented ShortTermServerStorage::new_operation

--- senc/server/ShortTermServerStorage.cpp
+++ senc/server/ShortTermServerStorage.cpp
@@ -10,5 +10,19 @@
 
 namespace senc::server
 {
+	OperationID ShortTermServerStorage::new_operation(const std::string& requester)
+	{
+		OperationID opid = OperationID::generate();
+		{
+			const std::lock_guard<std::mutex> lock(_mtxOperations);
 
+			// make sure operation ID is unique
+			while (_operations.contains(opid))
+				opid = OperationID::generate();
+
+			// register operation
+			_operations.insert(std::make_pair(opid, requester));
+		}
+		return opid;
+	}
 }
