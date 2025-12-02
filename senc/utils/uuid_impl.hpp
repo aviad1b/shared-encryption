d6c2368d7a02784655e52890e94acb835fbda73b
aviad1b <aviad.bloch10@gmail.com>
2025-12-02 21:42:34 +0200

    Chore: Added noexcepts to UUID::generate templated variations

--- senc/utils/uuid_impl.hpp
+++ senc/utils/uuid_impl.hpp
@@ -11,6 +11,7 @@
 namespace senc::utils
 {
 	inline UUID::Self UUID::generate(Callable<bool, const Self&> auto&& existsPred)
+		noexcept(CallableNoExcept<std::remove_cvref_t<decltype(existsPred)>, bool, const Self&>)
 	{
 		Self res = generate();
 		while (existsPred(res))
@@ -19,6 +20,7 @@ namespace senc::utils
 	}
 
 	inline UUID::Self UUID::generate(const HasContainsMethod<Self> auto& container)
+		noexcept(HasContainsMethodNoExcept<std::remove_cvref_t<decltype(container)>, Self>)
 	{
 		return generate([&container](const Self& self) { return container.contains(self); });
 	}
