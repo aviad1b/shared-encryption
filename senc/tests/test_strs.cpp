7e02fc7e8e1b5f2141c59e332d3b56e6a9b66fe5
aviad1b <aviad.bloch10@gmail.com>
2025-12-13 23:51:25 +0200

    Test: Added string test for modint

---
+++ senc/tests/test_strs.cpp
@@ -0,0 +1,29 @@
+/*********************************************************************
+ * \file   test_strs.cpp
+ * \brief  Contains tests for string-related utilities.
+ * 
+ * \author aviad1b
+ * \date   December 2025, Kislev 5786
+ *********************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "../utils/ModInt.hpp"
+#include "../utils/strs.hpp"
+
+using senc::utils::IntegralModTraits;
+using senc::utils::ModInt;
+
+using senc::utils::from_string;
+using senc::utils::to_string;
+
+TEST(Strs, ModInt)
+{
+	constexpr int modulus = 7;
+	using MI = ModInt<IntegralModTraits<int, modulus, true>>;
+	for (int i = 0; i < modulus; ++i)
+	{
+		MI val = i;
+		EXPECT_EQ(from_string<MI>(to_string(val)), val);
+	}
+}
