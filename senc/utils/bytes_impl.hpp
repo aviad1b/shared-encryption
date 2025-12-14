21fbe0da76e16c8ab0d685dc1a4575081ad384da
aviad1b <aviad.bloch10@gmail.com>
2025-12-14 03:03:26 +0200

    Fix: Added missing requires clause

--- senc/utils/bytes_impl.hpp
+++ senc/utils/bytes_impl.hpp
@@ -35,6 +35,8 @@ namespace senc::utils
 	}
 
 	template <std::ranges::input_range R>
+	requires (std::same_as<std::ranges::range_value_t<R>, byte> &&
+		!HasByteData<R>)
 	std::string bytes_to_base64(R&& rng)
 	{
 		return bytes_to_base64(Buffer(std::ranges::begin(rng), std::ranges::end(rng)));
