b27710128d3633f9bf563f996a91a6a21977d270
aviad1b <aviad.bloch10@gmail.com>
2025-12-15 04:11:27 +0200

    Doc: Added doc for server tests

--- senc/tests/test_server.cpp
+++ senc/tests/test_server.cpp
@@ -1,3 +1,11 @@
+/*********************************************************************
+ * \file   test_server.cpp
+ * \brief  Contains tests for server.
+ * 
+ * \author aviad1b
+ * \date   December 2025, Kislev 5786
+ *********************************************************************/
+
 #include <gtest/gtest.h>
 #include <functional>
 #include <memory>
