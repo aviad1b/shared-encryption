2ede921272866b607b334c8a5abf2000d30169c1
aviad1b <aviad.bloch10@gmail.com>
2025-12-23 03:57:28 +0200

    Fix: Made server skip initiator when registering lookup updates

--- senc/server/ConnectedClientHandler.cpp
+++ senc/server/ConnectedClientHandler.cpp
@@ -121,7 +121,8 @@ namespace senc::server
 		);
 
 		// inform all relevant members of lookup
-		auto members = utils::views::join(info.owners, info.reg_members);
+		auto members = utils::views::join(info.owners, info.reg_members) |
+			std::views::filter([this](const std::string& s) { return s != _username; });
 		for (const auto& member : members)
 			_updateManager.register_lookup(member, opid);
 
