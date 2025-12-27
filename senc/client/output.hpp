21b8e1415dad76523ea2f5d2d816335b3cd4ee11
aviad1b <aviad.bloch10@gmail.com>
2025-12-27 23:15:11 +0200

    Doc: Added documentation of all methods & concepts in output.hpp

--- senc/client/output.hpp
+++ senc/client/output.hpp
@@ -12,11 +12,28 @@
 
 namespace senc::client
 {
+	/**
+	 * @brief Prints public keys.
+	 * @param pubKey1 Public key of first layer to print.
+	 * @param pubKey2 Public key of second layer to print.
+	 */
 	void print_pub_keys(const PubKey& pubKey1, const PubKey& pubKey2);
 
+	/**
+	 * @brief Prints a private key shard of the first layer.
+	 * @param shard Private key shard of the first layer to print.
+	 */
 	void print_priv_key1_shard(const PrivKeyShard& shard);
 	
+	/**
+	 * @brief Prints a private key shard of the second layer.
+	 * @param shard Private key shard of the second layer to print.
+	 */
 	void print_priv_key2_shard(const PrivKeyShard& shard);
 
+	/**
+	 * @brief Prints ciphertext.
+	 * @param ciphertext Ciphertext to print.
+	 */
 	void print_ciphertext(const Ciphertext& ciphertext);
 }
