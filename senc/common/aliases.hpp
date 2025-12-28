ae1a4121d03d159c031474d8683910b506ea0634
aviad1b <aviad.bloch10@gmail.com>
2025-12-28 04:40:27 +0200

    Doc: Added aliases doc to all aliases in aliases.hpp

--- senc/common/aliases.hpp
+++ senc/common/aliases.hpp
@@ -16,28 +16,77 @@
 
 namespace senc
 {
+	/**
+	 * @typedef senc::Schema
+	 * @brief Encryption schema used by both client and server.
+	 */
 	using Schema = utils::enc::HybridElGamal2L<
 		utils::ECGroup, utils::enc::AES1L, utils::enc::ECHKDF2L
 	>;
 
+	/**
+	 * @typedef senc::Shamir
+	 * @brief Holds implementations of Shamir utilities for threshold decryption.
+	 */
 	using Shamir = utils::ShamirHybridElGamal<
 		utils::ECGroup, utils::enc::AES1L, utils::enc::ECHKDF2L,
 		utils::BigInt
 	>;
 
+	/**
+	 * @typedef senc::PubKey
+	 * @brief Public key type (for encryption).
+	 */
 	using PubKey = utils::enc::PubKey<Schema>;
+
+	/**
+	 * @typedef PrivKey
+	 * @brief Private key type (for decryption).
+	 */
 	using PrivKey = utils::enc::PrivKey<Schema>;
 
+	// server and client use plaintexts that are `utils::Buffer`.
 	static_assert(std::same_as<utils::enc::Plaintext<Schema>, utils::Buffer>);
 
+	/**
+	 * @typedef Ciphertext
+	 * @brief Ciphertext type of encryption schema.
+	 */
 	using Ciphertext = utils::enc::Ciphertext<Schema>;
 
+	/**
+	 * @typedef senc::PrivKeyShard
+	 * @brief Shamir shard of a distributed private key.
+	 */
 	using PrivKeyShard = typename Shamir::Shard;
+
+	/**
+	 * @typedef senc::PrivKeyShardID
+	 * @brief ID of private key Shamir shard (x value).
+	 */
 	using PrivKeyShardID = typename Shamir::ShardID;
+
+	/**
+	 * @typedef senc::PrivKeyShardValue
+	 * @brief Value of private key Shamir shard (polynomial result, y value).
+	 */
 	using PrivKeyShardValue = typename Shamir::ShardValue;
 
+	/**
+	 * @typedef senc::DecryptionPart
+	 * @brief Type of an unjoined decryption part computed using Shamir utilities and a private key Shamir shard.
+	 */
 	using DecryptionPart = typename Shamir::Part;
 
+	/**
+	 * @typedef senc::UserSetID
+	 * @brief ID of userset.
+	 */
 	using UserSetID = utils::UUID;
+
+	/**
+	 * @typedef senc::OperationID
+	 * @brief ID of decryption operation.
+	 */
 	using OperationID = utils::UUID;
 }
