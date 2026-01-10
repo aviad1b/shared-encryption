d459d35a4e72589e6a0665eec40cce46c269a618
aviad1b <aviad.bloch10@gmail.com>
2026-01-10 05:22:12 +0200

    Doc: Added brief of files doc for class PBKDF2

--- senc/utils/pwd/PBKDF2.hpp
+++ senc/utils/pwd/PBKDF2.hpp
@@ -1,6 +1,6 @@
 /*********************************************************************
  * \file   PBKDF2.hpp
- * \brief  
+ * \brief  Header of PBKDF2 class.
  * 
  * \author aviad1b
  * \date   January 2026, Teveth 5786
