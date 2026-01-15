3ad7fc862575ef64228733c93a35fa932633a889
aviad1b <aviad.bloch10@gmail.com>
2026-01-15 08:35:48 +0200

    Chore: Added ILogger interface

---
+++ senc/server/ILogger.hpp
@@ -0,0 +1,24 @@
+/*********************************************************************
+ * \file   ILogger.hpp
+ * \brief  Contains ILogger interface.
+ * 
+ * \author aviad1b
+ * \date   January 2026, Teveth 5786
+ *********************************************************************/
+
+#pragma once
+
+#include <string>
+
+namespace senc::server
+{
+	/**
+	 * @interface senc::server::ILogger
+	 * @brief Interface of server message logging.
+	 */
+	class ILogger
+	{
+	public:
+		virtual void log_info(const std::string& msg) = 0;
+	};
+}
