3de7fd53614626a8785e05239adcb09367a418c9
aviad1b <aviad.bloch10@gmail.com>
2026-02-01 01:23:19 +0200

    Chore: Removed fixed string list (i'm not dealing with this)

--- senc/utils/FixedString.hpp
+++ senc/utils/FixedString.hpp
@@ -11,6 +11,7 @@
 #include <string_view>
 #include <algorithm>
 #include <concepts>
+#include <tuple>
 
 namespace senc::utils
 {
@@ -102,19 +103,19 @@ namespace senc::utils
 	namespace sfinae
 	{
 		template <typename T>
-		struct is_fixed_string : std::false_type { };
+		struct is_fixed_string_type : std::false_type { };
 
 		template <std::size_t n>
-		struct is_fixed_string<FixedString<n>> : std::true_type { };
+		struct is_fixed_string_type<FixedString<n>> : std::true_type { };
 	}
 
 	/**
-	 * @concept senc::utils::AnyFixedString
+	 * @concept senc::utils::FixedStringType
 	 * @brief Looks for any instantation of `utils::FixedString`.
 	 * @tparam Self Examined typename.
 	 */
 	template <typename Self>
-	concept AnyFixedString = sfinae::is_fixed_string<Self>::value;
+	concept FixedStringType = sfinae::is_fixed_string_type<Self>::value;
 
 	/**
 	 * @struct senc::utils::FixedStringConstant
@@ -132,70 +133,4 @@ namespace senc::utils
 	 * @brief Empty variation of FixedStringConstant.
 	 */
 	struct EmptyFixedStringConstant : FixedStringConstant<> { };
-
-	template <AnyFixedString... Strs>
-	struct FixedStringList
-	{
-		/**
-		 * @brief List's length (amount of strings in list).
-		 */
-		static constexpr std::size_t LEN = sizeof...(Strs);
-
-		/**
-		 * @brief Gets list's length.
-		 * @return Amount of strings in list.
-		 */
-		constexpr std::size_t size() const { return LEN; }
-
-		/**
-		 * @brief Checks if contains a given string.
-		 * @tparam Str String to look for in list.
-		 * @return `true` if `Str` is in the list, otherwise `false`.
-		 */
-		template <AnyFixedString Str>
-		consteval bool contains() const
-		{
-			return ((Strs == Str) || ...);
-		}
-	};
-
-	namespace sfinae
-	{
-		template <typename T>
-		struct is_fixed_string_list : std::false_type { };
-
-		template <AnyFixedString... Strs>
-		struct is_fixed_string_list<FixedStringList<Strs...>> : std::true_type { };
-	}
-
-	/**
-	 * @concept senc::utils::AnyFixedStringList
-	 * @brief Looks for any instantation of `FixedStringList`.
-	 * @tparam Self Examined typename.
-	 */
-	template <typename Self>
-	concept AnyFixedStringList = sfinae::is_fixed_string_list<Self>::value;
-
-	namespace sfinae
-	{
-		template <AnyFixedStringList List, AnyFixedString Str>
-		struct fixed_string_list_contains : std::false_type { };
-
-		template <AnyFixedString Str, AnyFixedString... Strs>
-		struct fixed_string_list_contains<FixedStringList<Strs...>, Str>
-			: std::bool_constant<
-				((Strs == Str) || ...)
-			> { };
-	}
-
-	/**
-	 * @concept senc::utils::FixedStringListWith
-	 * @brief Looks for a fixed string list which contains a specific fixed string.
-	 * @tparam Self Examined typename.
-	 * @tparam Str String to look for in list.
-	 */
-	template <typename Self, typename Str>
-	concept FixedStringListWith = AnyFixedStringList<Self> &&
-		AnyFixedString<Str> &&
-		sfinae::fixed_string_list_contains<Self, Str>::value;
 }
