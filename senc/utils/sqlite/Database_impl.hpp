8aa21e3fe21cdd17a8dd34971accff48ae3a1b5d
aviad1b <aviad.bloch10@gmail.com>
2026-02-12 22:17:47 +0200

    Refactor: Removed excessive Values...

--- senc/utils/sqlite/Database_impl.hpp
+++ senc/utils/sqlite/Database_impl.hpp
@@ -61,7 +61,7 @@ namespace senc::utils::sqlite
 			throw SQLiteException("Failed to run statement: " + sql);
 
 		// bind parameters
-		ParamUtils::bind_all<std::make_index_sequence<sizeof...(Values)>, Values...>(
+		ParamUtils::bind_all<std::make_index_sequence<sizeof...(Values)>>(
 			stmt, values...
 		);
 
