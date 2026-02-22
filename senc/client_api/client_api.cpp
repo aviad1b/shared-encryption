c0866b2015268d795345ccccff9a2105fd5b2c5b
aviad1b <aviad.bloch10@gmail.com>
2026-02-22 06:10:47 +0200

    Refactor: auto

--- senc/client_api/client_api.cpp
+++ senc/client_api/client_api.cpp
@@ -14,7 +14,7 @@ namespace api = senc::clientapi;
 
 void FreeHandle(std::uintptr_t handle)
 {
-	api::Handle* pHandle = reinterpret_cast<api::Handle*>(handle);
+	auto* pHandle = reinterpret_cast<api::Handle*>(handle);
 
 	if (pHandle->allocated())
 		delete pHandle;
@@ -22,21 +22,21 @@ void FreeHandle(std::uintptr_t handle)
 
 bool HasError(std::uintptr_t handle)
 {
-	api::Handle* pHandle = reinterpret_cast<api::Handle*>(handle);
+	auto* pHandle = reinterpret_cast<api::Handle*>(handle);
 
 	return pHandle->has_error();
 }
 
 const char* GetError(std::uintptr_t handle)
 {
-	api::Error* pError = reinterpret_cast<api::Error*>(handle);
+	auto* pError = reinterpret_cast<api::Error*>(handle);
 
 	return pError->what();
 }
 
 const char* GetString(std::uintptr_t handle)
 {
-	api::Value<std::string>* pValue = reinterpret_cast<api::Value<std::string>*>(handle);
+	auto* pValue = reinterpret_cast<api::Value<std::string>*>(handle);
 
 	return pValue->get().c_str();
 }
