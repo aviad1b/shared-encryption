d3fac870834853b1dfbf3f64d9a105b769f4dbd6
aviad1b <aviad.bloch10@gmail.com>
2026-02-22 03:34:07 +0200

    Feat: Added profile read logic

--- senc/client_api/storage/ProfileStorage.cpp
+++ senc/client_api/storage/ProfileStorage.cpp
@@ -10,10 +10,146 @@
 
 namespace senc::clientapi::storage
 {
+	ProfileStorageIterator::ProfileStorageIterator(const ProfileEncKey& key,
+												   ProfileInputFile& file,
+												   utils::file_pos_t pos)
+		: _key(key), _file(file), _pos(pos),
+		  _recordEncSize1(read_profile_record_enc_size(_file)),
+		  _recordEncSize2(read_profile_record_enc_size(_file)),
+		  _record(read_profile_record(_file, _key, _recordEncSize1, _recordEncSize2)) { }
+
+	bool ProfileStorageIterator::operator==(const Self& other) const
+	{
+		return (this->_pos == other._pos);
+	}
+
+	ProfileStorageIterator::Self& ProfileStorageIterator::operator++()
+	{
+		this->_pos = next_pos();
+		this->_file.get().set_pos(this->_pos);
+		this->_recordEncSize1 = read_profile_record_enc_size(_file);
+		this->_recordEncSize2 = read_profile_record_enc_size(_file);
+		this->_record = read_profile_record(_file, _key, _recordEncSize1, _recordEncSize2);
+	}
+
+	ProfileStorageIterator::Self ProfileStorageIterator::operator++(int)
+	{
+		return Self(_key, _file, next_pos());
+	}
+
+	ProfileStorageIterator::reference ProfileStorageIterator::operator*() const
+	{
+		return _record;
+	}
+
+	ProfileStorageIterator::pointer ProfileStorageIterator::operator->() const
+	{
+		return &_record;
+	}
+
+	utils::file_pos_t ProfileStorageIterator::next_pos() const
+	{
+		// next record starts after sizes and record ciphertext
+		return this->_pos + (2 * sizeof(profile_record_enc_size_t)) + _recordEncSize1 + _recordEncSize2;;
+	}
+
+	profile_record_enc_size_t ProfileStorageIterator::read_profile_record_enc_size(ProfileInputFile& file)
+	{
+		return file.read<profile_record_enc_size_t>();
+	}
+
+	ProfileRecord ProfileStorageIterator::read_profile_record(ProfileInputFile& file,
+															  const ProfileEncKey& key,
+															  profile_record_enc_size_t size1,
+															  profile_record_enc_size_t size2)
+	{
+		// read encrypted profile
+		ProfileEncCiphertext encProfile{};
+		auto& [enc1, enc2] = encProfile;
+		enc1.resize(size1);
+		enc2.resize(size2);
+		file.read(enc1.data(), size1);
+		file.read(enc2.data(), size2);
+
+		// decrypt
+		utils::Buffer profileBytes = schema().decrypt(encProfile, key);
+
+		// parse
+		return parse_profile_record(key, profileBytes);
+	}
+
+	ProfileRecord ProfileStorageIterator::parse_profile_record(const ProfileEncKey& key, utils::Buffer& data)
+	{
+		const auto end = data.end();
+		auto it = data.begin();
+
+		utils::byte flagsByte = 0;
+		it = utils::read_bytes(flagsByte, it, end);
+		const auto flags = ProfileRecordFlags::from_byte(flagsByte);
+
+		UserSetID usersetID{};
+		it = utils::read_bytes(usersetID, it, end);
+
+		PubKey regLayerPubKey{}, ownerLayerPubKey{};
+		{
+			utils::Buffer pubKeyBuff(PubKey::ENCODED_SIZE);
+			it = utils::read_bytes(pubKeyBuff, it, end);
+			regLayerPubKey = PubKey::decode(pubKeyBuff);
+			it = utils::read_bytes(pubKeyBuff, it, end);
+			ownerLayerPubKey = PubKey::decode(pubKeyBuff);
+		}
+
+		// this constant is true as shard IDs are currently sampled from [1,MAX_MEMBER_COUNT]
+		// TODO: move this constant elsewhere in future version
+		constexpr std::size_t SHARD_ID_MAX_SIZE = sizeof(member_count_t);
+
+		// this constant is true as shard values are always < modulus
+		// TODO: move this constant elsewhere in future version
+		const std::size_t SHARD_VALUE_MAX_SIZE = (PrivKeyShardValue::modulus() - 1).MinEncodedSize();
+
+		PrivKeyShard regLayerPrivKeyShard{};
+		utils::Buffer shardIdBuff(SHARD_ID_MAX_SIZE);
+		utils::Buffer shardValBuff(SHARD_VALUE_MAX_SIZE);
+		utils::BigInt shardValUnderlying{};
+		it = utils::read_bytes(shardIdBuff, it, end);
+		it = utils::read_bytes(shardValBuff, it, end);
+		regLayerPrivKeyShard.first.Decode(shardIdBuff.data(), shardIdBuff.size());
+		shardValUnderlying.Decode(shardValBuff.data(), shardValBuff.size());
+		regLayerPrivKeyShard.second = std::move(shardValUnderlying);
+
+		// if non-owner record, stop here - no more data to read
+		if (!flags.is_owner)
+			return ProfileRecord::reg(
+				std::move(usersetID),
+				std::move(regLayerPubKey), std::move(ownerLayerPubKey),
+				std::move(regLayerPrivKeyShard)
+			);
+
+		// otherwise, read remaining data (owner shard) then return
+		PrivKeyShard ownerLayerPrivKeyShard{};
+		it = utils::read_bytes(shardIdBuff, it, end);
+		it = utils::read_bytes(shardValBuff, it, end);
+		ownerLayerPrivKeyShard.first.Decode(shardIdBuff.data(), shardIdBuff.size());
+		shardValUnderlying.Decode(shardValBuff.data(), shardValBuff.size());
+		ownerLayerPrivKeyShard.second = std::move(shardValUnderlying);
+
+		return ProfileRecord::owner(
+			std::move(usersetID),
+			std::move(regLayerPubKey), std::move(ownerLayerPubKey),
+			std::move(regLayerPrivKeyShard), std::move(ownerLayerPrivKeyShard)
+		);
+	}
+
+	ProfileEncSchema& ProfileStorageIterator::schema()
+	{
+		static thread_local ProfileEncSchema schema;
+		return schema;
+	}
+
 	ProfileStorage::ProfileStorage(const std::string& username, const std::string& password)
 		: _key(derive_key(username, password)) { }
 
-	utils::enc::AES1L::Key ProfileStorage::derive_key(const std::string& username, const std::string& password)
+	ProfileEncKey ProfileStorage::derive_key(const std::string& username, const std::string& password)
 	{
 		using KDF = utils::pwd::PBKDF2<utils::enc::AES1L::KEY_SIZE>;
 		static thread_local KDF kdf;
--- senc/client_api/storage/ProfileStorage.hpp
+++ senc/client_api/storage/ProfileStorage.hpp
@@ -10,15 +10,17 @@
 
 #include "../../utils/pwd/PBKDF2.hpp"
 #include "../../utils/enc/AES1L.hpp"
+#include "../../utils/BinFile.hpp"
+#include "../../common/sizes.hpp"
 #include "ProfileRecord.hpp"
 
 namespace senc::clientapi::storage
 {
 	/**
-	 * @typedef senc::clientapi::storage::profile_record_size_t
-	 * @brief Size of profile record on stored memory.
+	 * @typedef senc::clientapi::storage::profile_record_enc_size_t
+	 * @brief Size of profile record encryption part on stored memory.
 	 */
-	using profile_record_size_t = std::uint64_t;
+	using profile_record_enc_size_t = std::uint16_t;
 
 	/**
 	 * @struct senc::clientapi::storage::ProfileRecordFlags
@@ -41,6 +43,82 @@ namespace senc::clientapi::storage
 		}
 	};
 
+	using ProfileInputFile = utils::BinFile<utils::AccessFlags::Read>;
+	using ProfileEncSchema = utils::enc::AES1L;
+	static_assert(std::same_as<utils::enc::Plaintext<ProfileEncSchema>, utils::Buffer>);
+	using ProfileEncKey = utils::enc::Key<ProfileEncSchema>;
+	using ProfileEncCiphertext = utils::enc::Ciphertext<ProfileEncSchema>;
+	static_assert(2 == std::tuple_size_v<ProfileEncCiphertext>, "Ciphertext type changed, requires refactor");
+
+	/**
+	 * @class senc::clientapi::storage::ProfileStorageIterator
+	 * @brief Used for iteration over profile storage.
+	 */
+	class ProfileStorageIterator
+	{
+	public:
+		using Self = ProfileStorageIterator;
+		using value_type = const ProfileRecord;
+		using reference = const ProfileRecord&;
+		using pointer = const ProfileRecord*;
+		using difference_type = std::ptrdiff_t;
+		using iterator_category = std::input_iterator_tag;
+		using iterator_concept = std::input_iterator_tag;
+
+		ProfileStorageIterator(const ProfileEncKey& key,
+							   ProfileInputFile& file,
+							   utils::file_pos_t pos = 0);
+
+		ProfileStorageIterator(const Self&) = default;
+
+		Self& operator=(const Self&) = default;
+
+		bool operator==(const Self& other) const;
+
+		Self& operator++();
+
+		Self operator++(int);
+
+		reference operator*() const;
+
+		pointer operator->() const;
+
+	private:
+		std::reference_wrapper<const ProfileEncKey> _key;
+		std::reference_wrapper<ProfileInputFile> _file;
+		utils::file_pos_t _pos;
+		profile_record_enc_size_t _recordEncSize1, _recordEncSize2;
+		ProfileRecord _record;
+
+		utils::file_pos_t next_pos() const;
+
+		static profile_record_enc_size_t read_profile_record_enc_size(ProfileInputFile& file);
+
+		static ProfileRecord read_profile_record(ProfileInputFile& file,
+												 const ProfileEncKey& key,
+												 profile_record_enc_size_t size1,
+												 profile_record_enc_size_t size2);
+
+		static ProfileRecord parse_profile_record(const ProfileEncKey& key, utils::Buffer& data);
+		// TODO: Add const once utils::read_bytes accepts const_iterator
+
+		static ProfileEncSchema& schema();
+	};
+
+	/**
+	 * @class senc::clientapi::storage::ProfileDataRange
+	 * @brief Used as a range iterating over a profile's data.
+	 */
+	class ProfileDataRange
+	{
+	public:
+		using File = utils::BinFile<utils::AccessFlags::Read>;
+
+	private:
+		std::reference_wrapper<utils::enc::AES1L::Key> _key;
+		File _file;
+	};
+
 	/**
 	 * @class senc::clientapi::storage::ProfileStorage
 	 * @brief Manages storage of client's profile.
@@ -59,7 +137,7 @@ namespace senc::clientapi::storage
 		ProfileStorage(const std::string& username, const std::string& password);
 
 	private:
-		utils::enc::AES1L::Key _key;
+		ProfileEncKey _key;
 
 		/**
 		 * @brief Derives key for profile access from username and password.
@@ -67,6 +145,6 @@ namespace senc::clientapi::storage
 		 * @param password Password to derive key from.
 		 * @return Derived key.
 		 */
-		static utils::enc::AES1L::Key derive_key(const std::string& username, const std::string& password);
+		static ProfileEncKey derive_key(const std::string& username, const std::string& password);
 	};
 }
