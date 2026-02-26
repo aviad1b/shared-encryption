5ad39bc2cda251931d7b92f9279e7715a61b2a2c
aviad1b <aviad.bloch10@gmail.com>
2026-02-27 01:01:04 +0200

    Doc: Added doc of IClient methods

--- senc/client_api/IClient.hpp
+++ senc/client_api/IClient.hpp
@@ -23,24 +23,69 @@ namespace senc::clientapi
 	public:
 		virtual ~IClient() { }
 
+		/**
+		 * @brief Signs up as a new username (and stays logged in).
+		 * @param username Username to signup with.
+		 * @param password Password to signup with.
+		 */
 		virtual void signup(const std::string& username, const std::string& password) = 0;
 
+		/**
+		 * @brief Logs in to server (and stays logged in).
+		 * @param username Username to login with.
+		 * @param password Password to login with.
+		 */
 		virtual void login(const std::string& username, const std::string& password) = 0;
 
+		/**
+		 * @brief Logs out of server.
+		 */
 		virtual void logout() = 0;
 
+		/**
+		 * @brief Creates a new userset.
+		 * @note Requires user to be logged in.
+		 * @param owners Usernames of userset's owner members.
+		 * @param regMembers Usernames of userset's non-owner members.
+		 * @param ownersThreshold Minimum amount of owners required for decryption.
+		 * @param regMembersThreshold Minimum amount of non-owners required for decryption.
+		 * @return Userset's ID.
+		 */
 		virtual UserSetID make_userset(utils::ranges::StringViewRange&& owners,
 									   utils::ranges::StringViewRange&& regMembers,
 									   member_count_t ownersThreshold,
 									   member_count_t regMembersThreshold) = 0;
 
+		/**
+		 * @brief Gets all usersets owned by user.
+		 * @note Requires user to be logged in.
+		 * @param callback Callback function to call on the ID of each userset owned by user.
+		 */
 		virtual void get_usersets(std::function<void(const UserSetID&)> callback) = 0;
 
+		/**
+		 * @brief Gets all members of a specific userset.
+		 * @note Requires user to be logged in.
+		 * @param usersetID ID of userset to get members of.
+		 * @param callback Callback function to call on username of each member.
+		 */
 		virtual void get_userset_members(const UserSetID& usersetID,
 										 std::function<void(const std::string&)> callback) = 0;
 
+		/**
+		 * @brief Encrypts a message under a userset.
+		 * @note Requires user to be logged in.
+		 * @param msg Message to encrypt.
+		 * @return Encrypted message.
+		 */
 		virtual utils::Buffer encrypt(const utils::Buffer& msg) = 0;
 
+		/**
+		 * @brief Queues a message decryption under a userset.
+		 * @param usersetID ID of userset to decrypt under.
+		 * @param ciphertext Encrypted message to decrypt.
+		 * @return Decryption operation ID.
+		 */
 		virtual OperationID decrypt(const UserSetID& usersetID, const utils::Buffer& ciphertext) = 0;
 	};
 }
