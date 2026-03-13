4fd07c29fd617f8f111208d51741f6df8fd1e439
aviad1b <aviad.bloch10@gmail.com>
2026-03-13 22:44:25 +0200

    Chore: Added KeyEvolver class

---
+++ senc/common/KeyEvolver.cpp
@@ -0,0 +1,42 @@
+/*********************************************************************
+ * \file   KeyEvolver.cpp
+ * \brief  Implementation of `KeyEvolver` class.
+ * 
+ * \author aviad1b
+ * \date   March 2026, Adar 5786
+ *********************************************************************/
+
+#include "KeyEvolver.hpp"
+
+#include "../utils/pseudo_random.hpp"
+#include "../utils/Random.hpp"
+
+namespace senc
+{
+	utils::BigInt KeyEvolver::sample_seed()
+	{
+		static thread_local auto dist =
+			utils::Random<utils::BigInt>::get_dist_below(PubKey::order());
+
+		return dist();
+	}
+
+	KeyEvolver::KeyEvolver(utils::BigInt&& seed) : _offset(std::move(seed)) { }
+
+	void KeyEvolver::advance_offset()
+	{
+		this->_offset = utils::pseudo_random(_offset, PubKey::order().MinEncodedSize());
+	}
+
+	void KeyEvolver::apply_offset(PubKey& key)
+	{
+		// key is g^s. change to g^(s+offset)
+		key *= PubKey::generator().pow(this->_offset);
+	}
+
+	void KeyEvolver::apply_offset(PrivKeyShard& shard)
+	{
+		// shard is (x,y). change to (x,y+offset)
+		shard.second += this->_offset;
+	}
+}
---
+++ senc/common/KeyEvolver.hpp
@@ -0,0 +1,90 @@
+/*********************************************************************
+ * \file   KeyEvolver.hpp
+ * \brief  Header of `KeyEvolver` class.
+ * 
+ * \author aviad1b
+ * \date   March 2026, Adar 5786
+ *********************************************************************/
+
+#pragma once
+
+#include "../utils/concepts.hpp"
+#include "../utils/math.hpp"
+#include "aliases.hpp"
+
+namespace senc
+{
+	/**
+	 * @class senc::KeyEvolver
+	 * @brief Encapsulates key evolution logic.
+	 */
+	class KeyEvolver
+	{
+	public:
+		using Self = KeyEvolver;
+
+		/**
+		 * @brief Samples seed for key evolver.
+		 * @return Sampled seed.
+		 */
+		static utils::BigInt sample_seed();
+
+		/**
+		 * @brief Constructs a new key evolver instance.
+		 * @param seed Initial seed used for key evolution.
+		 */
+		KeyEvolver(utils::BigInt&& seed);
+
+		/**
+		 * @brief Copy constructor of key evolver.
+		 */
+		KeyEvolver(const Self&) = default;
+
+		/**
+		 * @brief Copy assignment operator of key evolver.
+		 */
+		Self& operator=(const Self&) = default;
+
+		/**
+		 * @brief Move constructor of key evolver.
+		 */
+		KeyEvolver(Self&&) = default;
+
+		/**
+		 * @brief Move assignment operator of key evolver.
+		 */
+		Self& operator=(Self&&) = default;
+
+		/**
+		 * @brief Evolves public keys and private key shards.
+		 * @param args Public key and private key shard arguments (by ref).
+		 */
+		template <utils::OneOf<PubKey, PrivKeyShard>... Ts>
+		void operator()(Ts&... args)
+		{
+			(apply_offset(args), ...);
+			advance_offset();
+		}
+
+	private:
+		// next offset for polynomial, computed pseudo-randomly each iteration
+		utils::BigInt _offset;
+
+		/**
+		 * @brief Advances to next pseudo-random offset.
+		 */
+		void advance_offset();
+
+		/**
+		 * @brief Applies current offset on public key.
+		 * @param key Public key (by ref).
+		 */
+		void apply_offset(PubKey& key);
+
+		/**
+		 * @brief Applies current offset on private key shard.
+		 * @param shard Private key shard(by ref).
+		 */
+		void apply_offset(PrivKeyShard& shard);
+	};
+}
