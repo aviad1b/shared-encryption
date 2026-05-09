7fa8094aec280cfe071b594cf65480c4833ad412
aviad1b <aviad.bloch10@gmail.com>
2026-05-10 02:58:11 +0300

    Feat: Added display of initiator

--- senc/cli_client/main.cpp
+++ senc/cli_client/main.cpp
@@ -669,7 +669,7 @@ namespace senc::cli_client
 
 		for (const auto& finishedDec : finishedDecs)
 		{
-			cout << "Finished decryption " << finishedDec.opid << ": ";
+			cout << "Finished decryption " << finishedDec.opid << " from " << finishedDec.initiator << ": ";
 			string ynIn = input("Show as text? (y/n): ");
 			if ("y" == ynIn || "Y" == ynIn)
 				cout << std::string(finishedDec.msg.begin(), finishedDec.msg.end()) << endl;
