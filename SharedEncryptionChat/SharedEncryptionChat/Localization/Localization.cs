40baf9de659e75ce4dd1c0df1684a56d7425132a
aviad1b <aviad.bloch10@gmail.com>
2026-05-23 22:24:46 +0300

    Chore: Added Localization class

--- SharedEncryptionChat/SharedEncryptionChat/Localization/Localization.cs
+++ SharedEncryptionChat/SharedEncryptionChat/Localization/Localization.cs
@@ -1,6 +1,28 @@
-﻿namespace SharedEncryptionChat.Localization;
+﻿using System.Globalization;
 
-// TODO: Implement class
+namespace SharedEncryptionChat.Localization;
+
+/// <summary>
+/// Primary localization class: Retreives localized strings from resources.
+/// </summary>
 public class Localization
 {
+    /// <summary>
+    /// <see cref="SharedEncryptionChat.Localization.LanguageHolder"/> of current language.
+    /// </summary>
+    public static LanguageHolder LanguageHolder { get; } = new(Language.EnglishUS);
+
+    /// <summary>
+    /// Sets current language.
+    /// </summary>
+    /// <param name="language">New language to set.</param>
+    public static void SetLanguage(Language language) => LanguageHolder.Language = language;
+
+    /// <summary>
+    /// Gets localized string based on current language.
+    /// </summary>
+    /// <param name="key">Localized string key.</param>
+    /// <returns>Localized string, or <see langword="null"/> if not found.</returns>
+    public static string? GetString(string key)
+        => LanguageHolder.Language.Resources.GetString(key, CultureInfo.CurrentUICulture);
 }
