240e93c97998871abf1d511417b1ce81d4a40c48
aviad1b <aviad.bloch10@gmail.com>
2026-05-25 02:00:15 +0300

    Chore: Added MessageSide enum

---
+++ SharedEncryptionChat/SharedEncryptionChat/Models/MessageSide.cs
@@ -0,0 +1,9 @@
+﻿namespace SharedEncryptionChat.Models;
+
+/// <summary>
+/// Represents the side that a message came from (sent/received).
+/// </summary>
+public enum MessageSide
+{
+    Sent, Received
+}
