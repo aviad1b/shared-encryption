10f2669b6a9c6cf17f2d08c90c87cc360ae0bada
aviad1b <aviad.bloch10@gmail.com>
2026-05-30 01:44:03 +0300

    Chore: Added message classes

---
+++ SharedEncryptionChat/SharedEncryptionChat/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿namespace SharedEncryptionChat.Models;
+
+/// <summary>
+/// Represents a message.
+/// </summary>
+public class Message
+{
+    /// <summary>
+    /// Side that the message came from (sent/received).
+    /// </summary>
+    public required MessageSide Side { get; set; }
+
+    /// <summary>
+    /// Message sender (<see langword="null"/> for self).
+    /// </summary>
+    public required string? Sender { get; set; }
+}
---
+++ SharedEncryptionChat/SharedEncryptionChat/Models/TextMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace SharedEncryptionChat.Models;
+
+/// <summary>
+/// Represents a text message.
+/// </summary>
+public class TextMessage : Message
+{
+    /// <summary>
+    /// Message content (text).
+    /// </summary>
+    public required string Content { get; set; }
+}
