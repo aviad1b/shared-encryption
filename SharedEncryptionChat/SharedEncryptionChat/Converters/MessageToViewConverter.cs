81a830ad85e44ed71e54b886329fb84029ebb347
aviad1b <aviad.bloch10@gmail.com>
2026-05-30 21:37:19 +0300

    Chore: Added MessageToViewConverter

---
+++ SharedEncryptionChat/SharedEncryptionChat/Converters/MessageToViewConverter.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Data.Converters;
+using SharedEncryptionChat.Models;
+using System;
+using System.Globalization;
+
+namespace SharedEncryptionChat.Converters;
+
+/// <summary>
+/// Converts message to displaying view.
+/// </summary>
+public class MessageToViewConverter : IValueConverter
+{
+    /// <inheritdoc/>
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        // expecting a message value
+        if (value is not Message msg)
+            return null;
+        return msg.CreateView();
+    }
+
+    /// <inheritdoc/>
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
